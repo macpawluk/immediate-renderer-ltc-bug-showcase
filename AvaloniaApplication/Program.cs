ea1fc2d5927a3a37c7a26506b9d60008274e732d
macpawluk <macpawluk@gmail.com>
2020-01-12 10:24:46 +0100

    Showcase app for the bug found

---
+++ AvaloniaApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Logging.Serilog;
+
+namespace AvaloniaApplication
+{
+    internal class Program
+    {
+        public static void Main(string[] args) => BuildAvaloniaApp().Start<MainWindow>();
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToDebug()
+                .With(new Win32PlatformOptions
+                {
+                    UseDeferredRendering = false
+                });
+    }
+}
